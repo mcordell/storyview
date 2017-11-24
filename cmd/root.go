ae2f86dca39939772da37c200fa7470cab8c2358
Michael Cordell <mcordell@q-centrix.com>
2017-11-24 12:36:10 -0800

    Tidy command text

--- cmd/root.go
+++ cmd/root.go
@@ -2,11 +2,10 @@ package cmd
 
 import (
 	"fmt"
-	"os"
-
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"os"
 )
 
 var cfgFile string
@@ -14,13 +13,8 @@ var cfgFile string
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "storyview",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "View all the details of a JIRA story in one place!",
+	Long:  `View all the details of a JIRA story in one place!`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
