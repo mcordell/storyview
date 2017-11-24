b93943b4b6d53cdf98f0cf18af74f8ee20b5054b
Michael Cordell <mcordell@q-centrix.com>
2017-11-24 13:28:45 -0800

    Add circle package

---
+++ circle/circle.go
@@ -0,0 +1,36 @@
+package circle
+
+import (
+	"fmt"
+	"github.com/jszwedko/go-circleci"
+	"github.com/mcordell/storyview/config"
+	"github.com/mcordell/storyview/jira"
+	"github.com/pkg/errors"
+)
+
+// BuildClient creates a Circle CI client
+func BuildClient(creds *config.CircleConfiguration) *circleci.Client {
+	return &circleci.Client{Token: creds.Token}
+}
+
+// OneLineBuild returns a build as a single string line
+func OneLineBuild(b *circleci.Build) string {
+	return fmt.Sprintf("Num: %d, Status: %s, Outcome: %s | %s", b.BuildNum, b.Status, b.Outcome, b.BuildURL)
+}
+
+// GetBuilds gets the current builds from circle for a given branch
+func GetBuilds(client *circleci.Client, b jira.GithubBranch) (builds []*circleci.Build, err error) {
+	if b.Account == "" {
+		err = errors.New("Cannot get builds for empty branch")
+		return
+	}
+
+	return client.ListRecentBuildsForProject(
+		b.Account,
+		b.Repo,
+		b.Branch,
+		"",
+		-1,
+		0,
+	)
+}
