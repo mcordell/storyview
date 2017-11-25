dc863b1cfc10aa67087076c31bae9c08e8e599d3
Michael Cordell <mcordell@q-centrix.com>
2017-11-24 17:24:00 -0800

    Add basic fetching/printing behavior

--- cmd/fetch.go
+++ cmd/fetch.go
@@ -2,16 +2,95 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/mcordell/storyview/circle"
+	"github.com/mcordell/storyview/config"
+	"github.com/mcordell/storyview/jira"
 	"github.com/spf13/cobra"
 )
 
 // fetchCmd represents the fetch command
 var fetchCmd = &cobra.Command{
-	Use:   "fetch",
+	Use:   "fetch ISSUE",
 	Short: "Fetch a JIRA issue",
+	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("fetch called")
+		var (
+			jiraConfig   config.JIRAConfiguration
+			circleConfig config.CircleConfiguration
+			err          error
+		)
+
+		jiraConfig, err = config.JIRA()
+
+		if err != nil {
+			panic(err)
+		}
+
+		client, err := jira.NewJIRAClient(&jiraConfig)
+
+		if err != nil {
+			panic(err)
+		}
+
+		issue, err := jira.GetIssue(client, args[0])
+
+		if err != nil {
+			panic(err)
+		}
+
+		jira.PrintIssue(issue)
+
+		circleConfig, err = config.Circle()
+
+		if err != nil {
+			panic(err)
+		}
+
+		result, err := jira.GetIssuePullRequests(client, issue)
+
+		if err != nil {
+			panic(err)
+		}
+
+		printBranches(result.Branches)
+
+		printPullRequests(result.PullRequests, &circleConfig)
 	},
+}
+
+func printBranches(branches []*jira.Branch) {
+	if len(branches) == 0 {
+		return
+	}
+
+	spacer := "  "
+
+	fmt.Printf("%sBranches:\n", spacer)
+
+	for _, branch := range branches {
+		fmt.Printf("%s%s%s\n", spacer, spacer, branch.Name)
+	}
+}
+
+func printPullRequests(pullRequests []*jira.PullRequest, circleCredentials *config.CircleConfiguration) {
+	if len(pullRequests) == 0 {
+		return
+	}
+
+	spacer := "  "
+
+	fmt.Printf("%sPull Requests:\n", spacer)
+
+	for _, pullRequest := range pullRequests {
+		fmt.Printf("%s%s%s\n", spacer, spacer, pullRequest.OneLine())
+		circleClient := circle.BuildClient(circleCredentials)
+		builds, err := circle.GetBuilds(circleClient, pullRequest.SourceBranch())
+		if err == nil {
+			for _, build := range builds {
+				fmt.Printf("%s%s%s%s\n", spacer, spacer, spacer, circle.OneLineBuild(build))
+			}
+		}
+	}
 }
 
 func init() {
