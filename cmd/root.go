0673403bbe79406a0059f77446e54d80269aab52
Michael Cordell <mcordell@q-centrix.com>
2017-11-24 17:09:42 -0800

    Remove unused command flag

--- cmd/root.go
+++ cmd/root.go
@@ -37,9 +37,6 @@ func init() {
 	// will be global for your application.
 	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.storyview.yaml)")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
